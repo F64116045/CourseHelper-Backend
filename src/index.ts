7c14b63e05fb31d8430d326b12147b9d30548670
f64116045 <f64116045@gs.ncku.edu.tw>
2025-07-18 09:14:51 +0800

    fix: update CORS settings for production

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const cors = require('cors');
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: 'http://127.0.0.1:5173' , credentials: true, }));
+app.use(cors({ origin: 'https://course-project-eight-omega.vercel.app' , credentials: true, }));
 
 
 app.get('/', (req, res) => {
